e90561caaf384b4c6a331b1eec55989b08fd562a
solovevv1 <solovevvs1@gmail.com>
2023-05-17 07:48:55 +0530

    setting chats between 2 users

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,29 +1,35 @@
-import React from 'react'
+import { doc, onSnapshot } from 'firebase/firestore';
+import React, { useContext, useEffect, useState } from 'react'
+import { AuthContext } from '../context/AuthContext';
+import { db } from '../firebase';
 
 const Chats = () => {
+  const [chats, setChats] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+      return () => {
+        unsub();
+      };
+    }
+    currentUser.uid && getChats();
+  }, [currentUser.uid])
+
   return (
     <div className='chats'>
-      <div className="userChat">
-        <img src="https://media.istockphoto.com/id/1281998322/photo/portrait-of-young-angry-woman.jpg?s=170667a&w=0&k=20&c=-JscNJC-sc_F9HTHhON9WmBeXYpghz9rHadYcEC13T4=" alt="" />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>Hello</p>
+      {Object.entries(chats)?.map((chat) => (
+        <div className="userChat" key={chat[0]}>
+          <img src={chat[1].userInfo.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{chat[1].userInfo.displayName}</span>
+            <p>{chat[1].userInfo.lastMessage?.text}</p>
+          </div>
         </div>
-      </div>
-      <div className="userChat">
-        <img src="https://media.istockphoto.com/id/1281998322/photo/portrait-of-young-angry-woman.jpg?s=170667a&w=0&k=20&c=-JscNJC-sc_F9HTHhON9WmBeXYpghz9rHadYcEC13T4=" alt="" />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="userChat">
-        <img src="https://media.istockphoto.com/id/1281998322/photo/portrait-of-young-angry-woman.jpg?s=170667a&w=0&k=20&c=-JscNJC-sc_F9HTHhON9WmBeXYpghz9rHadYcEC13T4=" alt="" />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>Hello</p>
-        </div>
-      </div>
+      ))};
     </div>
   )
 }
